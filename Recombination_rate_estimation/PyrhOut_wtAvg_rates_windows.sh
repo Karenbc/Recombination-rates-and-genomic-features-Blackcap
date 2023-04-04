7dcb18aeafa3e5422da0bafe36f3a9a51cffcb1f
Karen Bascon Cardozo <karenbc.22@gmail.com>
2023-04-04 20:12:39 +0200

    Update PyrhOut_wtAvg_rates_windows.sh

--- Recombination_rate_estimation/PyrhOut_wtAvg_rates_windows.sh
+++ Recombination_rate_estimation/PyrhOut_wtAvg_rates_windows.sh
@@ -1,13 +1,29 @@
-#With pyrho output calculate weighted average recombination rate in non-overlapping windows using python script
+#Calculate from pyrho output the weighted average recombination rate in non-overlapping windows using a python script
 
-In_rec=/home/bascon.c/PYRHO/Optimize_RecRates/NEW_Ref/Continent_Residents/Cont_Res0
+#Input file (rec rates converted to cM/Mb)
+In_rec=/home/Cont_Res0
+#Path for output file
 Rec_bin=/home/bascon.c/Recombination_Analysis/New_Ref/Rec_Bins_AvrgWeight
-script=/home/bascon.c/scripts/Recombination_Analisis/Rec_Rates
+#Script to calculate rec rates in windows
+script=/home/scripts/Recombination_Analisis/Rec_Rates
 
-
+#Modules:
 module load python/3.6.0 
+
+
+#Code:
+
+#50kb windows
+
+$script/Rec_windows.py  $In_rec/WholeGenome_chr_ContRes0_n38_Pen20_W50 50000 > $Rec_bin/OutPyrho_r_ContRes0_pen20W50_AvrgWgth_50kb
+
 
 #200kb windows
 
 $script/Rec_windows.py  $In_rec/WholeGenome_chr_ContRes0_n38_Pen20_W50 200000 > $Rec_bin/OutPyrho_r_ContRes0_pen20W50_AvrgWgth_200kb
+
+
+#1Mb windows
+
+$script/Rec_windows.py  $In_rec/WholeGenome_chr_ContRes0_n38_Pen20_W50 1000000 > $Rec_bin/OutPyrho_r_ContRes0_pen20W50_AvrgWgth_1Mb
 
