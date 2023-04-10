8e211c51922a39a6843c384f8dff8557cc8a7aa4
Karen Bascon Cardozo <karenbc.22@gmail.com>
2023-04-10 21:00:18 +0200

    Add files via upload

---
+++ Genomic features/CpGidentification_CpGcluster.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+
+
+#####CpGisland identification from blackcap reference genome#####
+
+
+
+#Tools:
+module load perl/5.24.1
+samtools
+
+#Folders
+
+#Path of the reference genome
+IN=/home/bascon.c/Reference_genome/NEW_Reference_genome
+#The output should be a directory  containing sequence files in FASTA format for each chromosmes
+OUT=/home/bascon.c/CpG_GC/CpGcluster/NEW_Ref/chr_ss_bSylAtr1_1_fasta
+
+
+
+
+## Extract chromosomes in fasta from the reference genome 
+
+for a in  1 2 3 Z 4 5 6 7 8 9 10 11 12 W 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 w_unkown 33 
+
+do
+samtools faidx $IN/bSylAtr1.1.fasta chr_${a} > $OUT/chr${a}.fa
+
+echo "chr_${a} done"
+
+done
+
+
+##Run the program with recommended parameters (d=50, p-value=1e-5)
+
+#perl CpGcluster.pl <assembly>  <d>  <P-value>
+
+#d is the threshold distance on basis of a given percentile. It is recommendable to use 50 (median distance)
+#P-value is the maximal P-value under which a CpG cluster is considered as a CpG island. The recommended limit is 1E-5
+
+perl ~/Bin/CpGcluster/CpGcluster.pl $OUT 50 1E-5
+
+echo "CpGcluster chromosomes done"
+
+
+
+
+
+##Merge chromosoms##
+
+#Remove first line (header) of CpG cluster output
+tail -n +2 $OUT/chr${a}_p50_CpGcluster.txt > $OUT/chr${a}_p50_CpGcluster_nohead.txt
+
+#Add chromosome name
+
+awk -v OFS='\t' {'print "chr_'"${a}"'",$2,$3'} $OUT/chr${a}_p50_CpGcluster_nohead.txt> $OUT/chr${a}_p50_CpGcluster.chr
+
+
+done
+
+awk 'NF' $OUT/chr*_p50_CpGcluster.chr > All_chr_p50_CpGcluster_chrname_merged_head
+
+
+
+
+
+##Filtering CpGi from CpGcluster output according to the length##
+
+
+#Filter out CpGs shorter or equal to 50bp
+awk '$5 >= 50 {print $0}' All_chr_p50_CpGcluster_chrname_merged_head > All_chr_p50_CpGcluster_chrname_noheader_merged_head_Filt_50Lng
