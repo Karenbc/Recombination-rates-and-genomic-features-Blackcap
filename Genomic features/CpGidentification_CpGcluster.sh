967eb42037eac476a32dffc37b25cca310c318b6
Karen Bascon Cardozo <karenbc.22@gmail.com>
2023-04-10 21:07:26 +0200

    Update CpGidentification_CpGcluster.sh

--- Genomic features/CpGidentification_CpGcluster.sh
+++ Genomic features/CpGidentification_CpGcluster.sh
@@ -2,25 +2,27 @@
 #
 
 
-#####CpGisland identification from blackcap reference genome#####
+#####CpGisland identification from blackcap reference genome using the program CpGcluster#####
 
 
 
 #Tools:
 module load perl/5.24.1
 samtools
+CpGcluster
 
 #Folders
 
-#Path of the reference genome
+#Reference genome path
 IN=/home/bascon.c/Reference_genome/NEW_Reference_genome
+
 #The output should be a directory  containing sequence files in FASTA format for each chromosmes
 OUT=/home/bascon.c/CpG_GC/CpGcluster/NEW_Ref/chr_ss_bSylAtr1_1_fasta
 
 
 
 
-## Extract chromosomes in fasta from the reference genome 
+## Extract chromosomes in FASTA format from the reference genome 
 
 for a in  1 2 3 Z 4 5 6 7 8 9 10 11 12 W 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 w_unkown 33 
 
@@ -36,8 +38,9 @@ done
 
 #perl CpGcluster.pl <assembly>  <d>  <P-value>
 
-#d is the threshold distance on basis of a given percentile. It is recommendable to use 50 (median distance)
-#P-value is the maximal P-value under which a CpG cluster is considered as a CpG island. The recommended limit is 1E-5
+    #d is the threshold distance on basis of a given percentile. It is recommendable to use 50 (median distance)
+    #P-value is the maximal P-value under which a CpG cluster is considered as a CpG island. The recommended limit is 1E-5
+
 
 perl ~/Bin/CpGcluster/CpGcluster.pl $OUT 50 1E-5
 
@@ -47,14 +50,18 @@ echo "CpGcluster chromosomes done"
 
 
 
-##Merge chromosoms##
+##Merge chromosomes##
+
+for a in  1 2 3 Z 4 5 6 7 8 9 10 11 12 W 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 w_unkown 33 
+
+do
 
 #Remove first line (header) of CpG cluster output
 tail -n +2 $OUT/chr${a}_p50_CpGcluster.txt > $OUT/chr${a}_p50_CpGcluster_nohead.txt
 
 #Add chromosome name
 
-awk -v OFS='\t' {'print "chr_'"${a}"'",$2,$3'} $OUT/chr${a}_p50_CpGcluster_nohead.txt> $OUT/chr${a}_p50_CpGcluster.chr
+awk -v OFS='\t' {'print "chr_'"${a}"'",$2,$3'} $OUT/chr${a}_p50_CpGcluster_nohead.txt > $OUT/chr${a}_p50_CpGcluster.chr
 
 
 done
